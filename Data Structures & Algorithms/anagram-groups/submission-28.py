e5bb71a77e78980327216de9c0f05bcea0018864
jason nguyen <jasonisnot0@gmail.com>
2026-08-22 13:12:09 -0400

    Add: anagram-groups - submission-28

---
+++ Data Structures & Algorithms/anagram-groups/submission-28.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        sol = defaultdict(list)
+        for word in strs:
+            temp = [0] * 26
+            for char in word:
+                temp[ord(char) - ord('a')] += 1
+
+            temp = tuple(temp)
+            sol[temp].append(word)
+            
+
+        return list(sol.values())
