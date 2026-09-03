46eadaaaae034d91ff2d46c7dafacb2d2ce0e761
jason nguyen <jasonisnot0@gmail.com>
2026-09-03 11:33:14 -0400

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.py
@@ -0,0 +1,40 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        array1, array2 = [0] * 26, [0] * 26
+
+        for i in range(len(s1)):
+            array1[ord(s1[i]) - ord('a')] += 1
+            array2[ord(s2[i]) - ord('a')] += 1
+
+        matches = 0
+
+        for j in range(26):
+            if array1[j] == array2[j]:
+                matches += 1
+
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+
+            index = ord(s2[r]) - ord('a')
+            array2[index] += 1
+            if array1[index] == array2[index]:
+                matches += 1
+            elif array1[index] + 1 == array2[index]:
+                matches -= 1
+
+            index = ord(s2[r - len(s1)]) - ord('a')
+            array2[index] -= 1
+            if array1[index] == array2[index]:
+                matches += 1
+            elif array1[index] - 1 == array2[index]:
+                matches -= 1
+
+        if matches == 26:
+            return True
+
+        else:
+            return False
